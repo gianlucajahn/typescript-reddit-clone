605461876df87dc897a640062569e8b2f2531c45
Gianluca Jahn <gianluca.jahn98@gmail.com>
2022-08-21 20:17:18 +0200

    Testing github language statistics

--- src/containers/Home/Home.tsx
+++ src/containers/Home/Home.tsx
@@ -9,6 +9,26 @@ export default function Home (props: HomeProps) {
   return (
     <div>
       <h1>This is the home page.</h1>
+      <h1>This is the home page.</h1>
+      <h1>This is the home page.</h1>
+      <h1>This is the home page.</h1>
+      <h1>This is the home page.</h1>
+      <h1>This is the home page.</h1>
+      <h1>This is the home page.</h1>
+      <h1>This is the home page.</h1>
+      <h1>This is the home page.</h1>
+      <h1>This is the home page.</h1>
+      <h1>This is the home page.</h1>
+      <h1>This is the home page.</h1>
+      <h1>This is the home page.</h1>
+      <h1>This is the home page.</h1>
+      <h1>This is the home page.</h1>
+      <h1>This is the home page.</h1>
+      <h1>This is the home page.</h1>
+      <h1>This is the home page.</h1>
+      <h1>This is the home page.</h1>
+      <h1>This is the home page.</h1>
+      <h1>This is the home page.</h1>
     </div>
   );
 }
