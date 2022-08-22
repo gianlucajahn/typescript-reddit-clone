85a89337e592a27c7ab644ca9becaa2289d1b3d0
Gianluca Jahn <gianluca.jahn98@gmail.com>
2022-08-22 18:56:04 +0200

    Add logged out navbar icons

--- src/containers/Home/Home.tsx
+++ src/containers/Home/Home.tsx
@@ -7,28 +7,7 @@ export interface HomeProps {
 
 export default function Home (props: HomeProps) {
   return (
-    <div>
-      <h1>This is the home page.</h1>
-      <h1>This is the home page.</h1>
-      <h1>This is the home page.</h1>
-      <h1>This is the home page.</h1>
-      <h1>This is the home page.</h1>
-      <h1>This is the home page.</h1>
-      <h1>This is the home page.</h1>
-      <h1>This is the home page.</h1>
-      <h1>This is the home page.</h1>
-      <h1>This is the home page.</h1>
-      <h1>This is the home page.</h1>
-      <h1>This is the home page.</h1>
-      <h1>This is the home page.</h1>
-      <h1>This is the home page.</h1>
-      <h1>This is the home page.</h1>
-      <h1>This is the home page.</h1>
-      <h1>This is the home page.</h1>
-      <h1>This is the home page.</h1>
-      <h1>This is the home page.</h1>
-      <h1>This is the home page.</h1>
-      <h1>This is the home page.</h1>
+    <div className="home">
     </div>
   );
 }
--- src/containers/NavBar/NavBar.tsx
+++ src/containers/NavBar/NavBar.tsx
@@ -1,5 +1,10 @@
 import * as React from 'react';
 import './NavBar.scss';
+import { ReactComponent as RedditLogo } from "../../resources/images/redditlogo.svg";
+import { ReactComponent as Reddit } from "../../resources/images/reddit.svg";
+import { ReactComponent as Search } from "../../resources/images/search.svg";
+import { ReactComponent as Expand } from "../../resources/images/expand.svg";
+import { ReactComponent as User } from "../../resources/images/user.svg";
 
 export interface NavBarProps {
 
@@ -7,8 +12,30 @@ export interface NavBarProps {
 
 export default function NavBar (props: NavBarProps) {
   return (
-    <div>
-      <h3>This is the nav bar.</h3>
+    <div className="navBar">
+        <div className="logo">
+          <RedditLogo className="redditLogo" />
+          <Reddit className="reddit" />
+        </div>
+
+        <div className="search">
+          <input type="text" placeholder="Reddit durchsuchen" className="searchBar">
+          </input>
+        </div>
+
+        <div className="user">
+            <button className="auth login">
+              Login
+            </button>
+            <button className="auth register">
+              Register
+            </button>
+  
+            <div className="userInfo">
+              <img src={require("../../resources/images/user.png")} className="userImg" />
+              <img src={require("../../resources/images/expand.png")} className="expand" />
+            </div>
+        </div>
     </div>
   );
 }
