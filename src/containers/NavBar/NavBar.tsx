91a6ce8db9acc9f025da98aa031dd299f55fb4ac
Gianluca Jahn <gianluca.jahn98@gmail.com>
2022-08-24 18:23:26 +0200

    Finish nested dropdown menu für discovery category

--- src/containers/NavBar/NavBar.tsx
+++ src/containers/NavBar/NavBar.tsx
@@ -20,6 +20,7 @@ export default function NavBar (props: NavBarProps) {
     celebrity: false,
     business: false,
     crypto: false,
+    mehr: false,
     weitereinfos: false,
     richtlinien: true
   });
@@ -35,7 +36,8 @@ export default function NavBar (props: NavBarProps) {
         celebrity: false,
         business: false,
         crypto: false,
-        weitereinfos: false,
+        mehr: false,
+        weitereinfos: dropdownState.weitereinfos,
         richtlinien: dropdownState.richtlinien
       })} else {
         setDropdownState({...dropdownState, [changedState]: !dropdownState[changedState as keyof typeof dropdownState]});
@@ -96,7 +98,7 @@ export default function NavBar (props: NavBarProps) {
 
                 <div className="dropdownItem expanded" style={{ display: dropdownState.erkunden ? "flex" : "none" }} id="gaming" onClick={handleExpand}>
                   <h3>Gaming</h3>
-                  <img className="icon expand" src={require("../../resources/images/expandblack.png")} />
+                  <img className={ dropdownState.gaming ? "expanded" : "expand" } src={require("../../resources/images/expandblack.png")} />
                 </div>
 
                 <div className="dropdownItem nested" style={{ display: dropdownState.gaming ? "flex" : "none" }}>
@@ -135,38 +137,242 @@ export default function NavBar (props: NavBarProps) {
                   <h3>Escape from Tarkov</h3>
                 </div>
 
-                <div className="dropdownItem nested" style={{ display: dropdownState.gaming ? "flex" : "none" }}>
+                <div className="dropdownItem nested lastChild" style={{ display: dropdownState.gaming ? "flex" : "none" }}>
                   <h3>Watch Dogs: Legion</h3>
                 </div>
 
-                <div className="dropdownItem expanded" style={{ display: dropdownState.erkunden ? "flex" : "none" }}>
+                <div className="dropdownItem expanded" style={{ display: dropdownState.erkunden ? "flex" : "none" }} id="sports" onClick={handleExpand}>
                   <h3>Sports</h3>
-                  <img className="icon expand" src={require("../../resources/images/expandblack.png")} />
+                  <img className={ dropdownState.sports ? "expanded" : "expand" } src={require("../../resources/images/expandblack.png")} />
                 </div>
 
-                <div className="dropdownItem expanded" style={{ display: dropdownState.erkunden ? "flex" : "none" }}>
+                <div className="dropdownItem nested" style={{ display: dropdownState.sports ? "flex" : "none" }}>
+                  <h3>NFL</h3>
+                </div>
+
+                <div className="dropdownItem nested" style={{ display: dropdownState.sports ? "flex" : "none" }}>
+                  <h3>NBA</h3>
+                </div>
+
+                <div className="dropdownItem nested" style={{ display: dropdownState.sports ? "flex" : "none" }}>
+                  <h3>Atlanta Hawks</h3>
+                </div>
+
+                <div className="dropdownItem nested" style={{ display: dropdownState.sports ? "flex" : "none" }}>
+                  <h3>Los Angeles Lakers</h3>
+                </div>
+
+                <div className="dropdownItem nested" style={{ display: dropdownState.sports ? "flex" : "none" }}>
+                  <h3>Boston Celtics</h3>
+                </div>
+
+                <div className="dropdownItem nested" style={{ display: dropdownState.sports ? "flex" : "none" }}>
+                  <h3>Arsenal F.C.</h3>
+                </div>
+
+                <div className="dropdownItem nested" style={{ display: dropdownState.sports ? "flex" : "none" }}>
+                  <h3>Philadelphia 76ers</h3>
+                </div>
+
+                <div className="dropdownItem nested" style={{ display: dropdownState.sports ? "flex" : "none" }}>
+                  <h3>Premier League</h3>
+                </div>
+
+                <div className="dropdownItem nested lastChild" style={{ display: dropdownState.sports ? "flex" : "none" }}>
+                  <h3>UFC</h3>
+                </div>
+
+                <div className="dropdownItem expanded" style={{ display: dropdownState.erkunden ? "flex" : "none" }} id="television" onClick={handleExpand}>
                   <h3>Television</h3>
-                  <img className="icon expand" src={require("../../resources/images/expandblack.png")} />
+                  <img className={ dropdownState.television ? "expanded" : "expand" } src={require("../../resources/images/expandblack.png")} />
                 </div>
 
-                <div className="dropdownItem expanded" style={{ display: dropdownState.erkunden ? "flex" : "none" }}>
+                <div className="dropdownItem nested" style={{ display: dropdownState.television ? "flex" : "none" }}>
+                  <h3>Game of Thrones</h3>
+                </div>
+
+                <div className="dropdownItem nested" style={{ display: dropdownState.television ? "flex" : "none" }}>
+                  <h3>Breaking Bad</h3>
+                </div>
+
+                <div className="dropdownItem nested" style={{ display: dropdownState.television ? "flex" : "none" }}>
+                  <h3>Rick &#38; Morty</h3>
+                </div>
+
+                <div className="dropdownItem nested" style={{ display: dropdownState.television ? "flex" : "none" }}>
+                  <h3>The Walking Dead</h3>
+                </div>
+
+                <div className="dropdownItem nested" style={{ display: dropdownState.television ? "flex" : "none" }}>
+                  <h3>House of Cards</h3>
+                </div>
+
+                <div className="dropdownItem nested" style={{ display: dropdownState.television ? "flex" : "none" }}>
+                  <h3>Prison Break</h3>
+                </div>
+
+                <div className="dropdownItem nested" style={{ display: dropdownState.television ? "flex" : "none" }}>
+                  <h3>Sherlock</h3>
+                </div>
+
+                <div className="dropdownItem nested" style={{ display: dropdownState.television ? "flex" : "none" }}>
+                  <h3>Suits</h3>
+                </div>
+
+                <div className="dropdownItem nested lastChild" style={{ display: dropdownState.television ? "flex" : "none" }}>
+                  <h3>Dark</h3>
+                </div>
+
+                <div className="dropdownItem expanded" style={{ display: dropdownState.erkunden ? "flex" : "none" }} id="celebrity" onClick={handleExpand}>
                   <h3>Celebrity</h3>
-                  <img className="icon expand" src={require("../../resources/images/expandblack.png")} />
+                  <img className={ dropdownState.celebrity ? "expanded" : "expand" } src={require("../../resources/images/expandblack.png")} />
                 </div>
 
-                <div className="dropdownItem expanded" style={{ display: dropdownState.erkunden ? "flex" : "none" }}>
+                <div className="dropdownItem nested" style={{ display: dropdownState.celebrity ? "flex" : "none" }}>
+                  <h3>Tom Hiddleston</h3>
+                </div>
+
+                <div className="dropdownItem nested" style={{ display: dropdownState.celebrity ? "flex" : "none" }}>
+                  <h3>Benedict Cumberbatch</h3>
+                </div>
+
+                <div className="dropdownItem nested" style={{ display: dropdownState.celebrity ? "flex" : "none" }}>
+                  <h3>Mark Ruffalo</h3>
+                </div>
+
+                <div className="dropdownItem nested" style={{ display: dropdownState.celebrity ? "flex" : "none" }}>
+                  <h3>Robert Downey Jr.</h3>
+                </div>
+
+                <div className="dropdownItem nested" style={{ display: dropdownState.celebrity ? "flex" : "none" }}>
+                  <h3>Tom Holland</h3>
+                </div>
+
+                <div className="dropdownItem nested" style={{ display: dropdownState.celebrity ? "flex" : "none" }}>
+                  <h3>Zendaya</h3>
+                </div>
+
+                <div className="dropdownItem nested" style={{ display: dropdownState.celebrity ? "flex" : "none" }}>
+                  <h3>Chris Hemsworth</h3>
+                </div>
+
+                <div className="dropdownItem nested" style={{ display: dropdownState.celebrity ? "flex" : "none" }}>
+                  <h3>Scarlett Johansson</h3>
+                </div>
+
+                <div className="dropdownItem nested lastChild" style={{ display: dropdownState.celebrity ? "flex" : "none" }}>
+                  <h3>Samuel L. Jackson</h3>
+                </div>
+
+                <div className="dropdownItem expanded" style={{ display: dropdownState.erkunden ? "flex" : "none" }} id="business" onClick={handleExpand}>
                   <h3>Business</h3>
-                  <img className="icon expand" src={require("../../resources/images/expandblack.png")} />
+                  <img className={ dropdownState.business ? "expanded" : "expand" } src={require("../../resources/images/expandblack.png")} />
                 </div>
 
-                <div className="dropdownItem expanded" style={{ display: dropdownState.erkunden ? "flex" : "none" }}>
+                <div className="dropdownItem nested" style={{ display: dropdownState.business ? "flex" : "none" }}>
+                  <h3>Berkshire Hathaway</h3>
+                </div>
+
+                <div className="dropdownItem nested" style={{ display: dropdownState.business ? "flex" : "none" }}>
+                  <h3>Tesla</h3>
+                </div>
+
+                <div className="dropdownItem nested" style={{ display: dropdownState.business ? "flex" : "none" }}>
+                  <h3>SpaceX</h3>
+                </div>
+
+                <div className="dropdownItem nested" style={{ display: dropdownState.business ? "flex" : "none" }}>
+                  <h3>Best Buy</h3>
+                </div>
+
+                <div className="dropdownItem nested" style={{ display: dropdownState.business ? "flex" : "none" }}>
+                  <h3>Walgreens</h3>
+                </div>
+
+                <div className="dropdownItem nested" style={{ display: dropdownState.business ? "flex" : "none" }}>
+                  <h3>Nvidia</h3>
+                </div>
+
+                <div className="dropdownItem nested" style={{ display: dropdownState.business ? "flex" : "none" }}>
+                  <h3>Alphabet</h3>
+                </div>
+
+                <div className="dropdownItem nested" style={{ display: dropdownState.business ? "flex" : "none" }}>
+                  <h3>Apple</h3>
+                </div>
+
+                <div className="dropdownItem nested lastChild" style={{ display: dropdownState.business ? "flex" : "none" }}>
+                  <h3>GameStop</h3>
+                </div>
+
+                <div className="dropdownItem expanded" style={{ display: dropdownState.erkunden ? "flex" : "none" }} id="crypto" onClick={handleExpand}>
                   <h3>Crypto</h3>
-                  <img className="icon expand" src={require("../../resources/images/expandblack.png")} />
+                  <img className={ dropdownState.crypto ? "expanded" : "expand" } src={require("../../resources/images/expandblack.png")} />
                 </div>
 
-                <div className="dropdownItem expanded" style={{ display: dropdownState.erkunden ? "flex" : "none" }}>
+                <div className="dropdownItem nested" style={{ display: dropdownState.crypto ? "flex" : "none" }}>
+                  <h3>Cardano</h3>
+                </div>
+
+                <div className="dropdownItem nested" style={{ display: dropdownState.crypto ? "flex" : "none" }}>
+                  <h3>Dogecoin</h3>
+                </div>
+
+                <div className="dropdownItem nested" style={{ display: dropdownState.crypto ? "flex" : "none" }}>
+                  <h3>Algorand</h3>
+                </div>
+
+                <div className="dropdownItem nested" style={{ display: dropdownState.crypto ? "flex" : "none" }}>
+                  <h3>Bitcoin</h3>
+                </div>
+
+                <div className="dropdownItem nested" style={{ display: dropdownState.crypto ? "flex" : "none" }}>
+                  <h3>Ethereum</h3>
+                </div>
+
+                <div className="dropdownItem nested" style={{ display: dropdownState.crypto ? "flex" : "none" }}>
+                  <h3>Litecoin</h3>
+                </div>
+
+                <div className="dropdownItem nested" style={{ display: dropdownState.crypto ? "flex" : "none" }}>
+                  <h3>Bitcoin Cash</h3>
+                </div>
+
+                <div className="dropdownItem nested" style={{ display: dropdownState.crypto ? "flex" : "none" }}>
+                  <h3>Ripple</h3>
+                </div>
+
+                <div className="dropdownItem nested lastChild" style={{ display: dropdownState.crypto ? "flex" : "none" }}>
+                  <h3>XRP</h3>
+                </div>
+
+                <div className="dropdownItem expanded" style={{ display: dropdownState.erkunden ? "flex" : "none" }} id="mehr" onClick={handleExpand}>
                   <h3>Mehr</h3>
-                  <img className="icon expand" src={require("../../resources/images/expandblack.png")} />
+                  <img className={ dropdownState.mehr ? "expanded" : "expand" } src={require("../../resources/images/expandblack.png")} />
+                </div>
+
+                <div className="dropdownItem nested" style={{ display: dropdownState.mehr ? "flex" : "none" }}>
+                  <h3>Animals and Pets</h3>
+                </div>
+
+                <div className="dropdownItem nested" style={{ display: dropdownState.mehr ? "flex" : "none" }}>
+                  <h3>Anime</h3>
+                </div>
+
+                <div className="dropdownItem nested" style={{ display: dropdownState.mehr ? "flex" : "none" }}>
+                  <h3>Philosophy</h3>
+                </div>
+
+                <div className="dropdownItem nested" style={{ display: dropdownState.mehr ? "flex" : "none" }}>
+                  <h3>Fashion</h3>
+                </div>
+
+                <div className="dropdownItem nested" style={{ display: dropdownState.mehr ? "flex" : "none" }}>
+                  <h3>Hobbies</h3>
+                </div>
+
+                <div className="dropdownItem nested lastChild" style={{ display: dropdownState.mehr ? "flex" : "none" }}>
+                  <h3>Music</h3>
                 </div>
 
                 <div className="dropdownItem einstellungen">
