13c473db2eefce99b9e7bc6290f963a1040f0d31
Gianluca Jahn <gianluca.jahn98@gmail.com>
2022-10-24 19:27:16 +0200

    Create file structure for submit page container

---
+++ src/containers/SubmitPage/SubmitPage.tsx
@@ -0,0 +1,17 @@
+import React, { MouseEventHandler } from 'react';
+import './SubmitPage.scss';
+
+export interface SubmitPageProps {
+}
+
+export default function SubmitPage (props: SubmitPageProps) {
+  const {
+
+  } = props;
+
+  return (
+    <div className="submit-page">
+      
+    </div>
+  );
+}
